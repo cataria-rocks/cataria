5407f9b7c8ee483db3469340178da8b490fb741c
Maria Vershinina <marfuzzi@yandex-team.ru>
2017-09-01 15:07:00 +0300

    delete //

--- common.blocks/main/_view/main_view_blank.bemtree.js
+++ common.blocks/main/_view/main_view_blank.bemtree.js
@@ -1,57 +1,3 @@
-// block('main').mod('view', 'blank').content()(node => {
-//     return !node.data.user ? { block: 'user' } : {
-//         block: 'form',
-//         content: [
-//             {
-//                 block: 'heading',
-//                 content: 'Cataria'
-//             },
-//             [
-//                 {
-//                     label: 'URL исходного документа',
-//                     name: 'doc',
-//                     placeholder: 'https://github.com/cataria-rocks/cataria/blob/master/README.md'
-//                 },
-//                 {
-//                     label: 'Язык исходного документа',
-//                     name: 'sourceLang',
-//                     placeholder: 'en'
-//                 },
-//                 {
-//                     label: 'Файл перевода',
-//                     name: 'target',
-//                     placeholder: 'README.ru.md'
-//                 },
-//                 {
-//                     label: 'Язык перевода',
-//                     name: 'targetLang',
-//                     placeholder: 'ru'
-//                 }
-//             ].map(input => [
-//                 {
-//                     elem: 'label',
-//                     content: input.label,
-//                     'for': input.name
-//                 },
-//                 {
-//                     block: 'input',
-//                     mods: { theme: 'islands', size: 'm', width: 'available', 'has-clear': true },
-//                     name: input.name,
-//                     id: input.name,
-//                     placeholder: input.placeholder
-//                 }
-//             ]).concat({
-//                 block: 'button',
-//                 mods: { theme: 'islands', size: 'm', type: 'submit', view: 'action' },
-//                 text: 'Перевести'
-//             }).map(item => ({
-//                 elem: 'item',
-//                 content: item
-//             }))
-//         ]
-//     };
-// });
-
 block('main')(
     mod('view', 'blank').content()(node => {
     const block = node.block;
@@ -74,18 +20,16 @@ block('main')(
                     {
                         block,
                         elem: 'flex-wrapper',
-                     //   elemMods: { column: 2 }, //__flex-wrapper_column_2
                         content: [
                             {
-                                elem: 'item', // __flex-wrapper_column_2 __iem:first-child { padding-right: 100500px }
+                                elem: 'item',
                                 elemMods: { column: 2 },
                                 label: 'Язык исходного документа',
                                 name: 'sourceLang',
                                 placeholder: 'en'
                             },
                             {
-
-                                elem: 'item', // __flex-wrapper_column_2 __iem + __iem { padding-left: 100500px }
+                                elem: 'item',
                                 elemMods: { column: 2 },
                                 label: 'Язык перевода',
                                 name: 'targetLang',
