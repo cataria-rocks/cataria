154c98f2e078b24d3f22514ec580be4eb22de7ad
aisel01 <aiselsherfedinova@yandex.ru>
2017-09-18 18:01:05 +0300

    fixed bug with alternative translations

--- common.blocks/segments/segments.bemhtml.js
+++ common.blocks/segments/segments.bemhtml.js
@@ -1,10 +1,14 @@
 block('segments').replace()(function() {
     return {
         tag: 'script',
-        content: 'var segments = ' + JSON.stringify(this.ctx.segments.map(segment => {
-            segment.altTrans = applyCtx(segment.altTrans);
-
-            return segment;
-        }))
+        content: {
+            html: 'var segments = ' +
+                JSON.stringify(this.ctx.segments.map(segment => {
+                    if (typeof segment.altTrans === 'object') {
+                        segment.altTrans = applyCtx(segment.altTrans);
+                    }
+                    return segment;
+                }))
+        }
     };
 });
